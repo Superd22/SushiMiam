77b32ab96529e79f22fe1948de9bb3521639f93e
Superd22 <superd001@gmail.com>
2015-02-20 17:40:00 +0100

    v0.32
    implé states +  début stocks

--- src/Player.java
+++ src/Player.java
@@ -23,8 +23,9 @@ public class Player {
 	// 0 = Shrimp / 1 = Riz / 2 = Feuille Verte / 3 = fish egg / 4 = salmon / 5 = unagi 
 	private int[] stocks = new int[]{5,10,10,10,5,5};
 	private int[][] sushiPosition = new int[][]{{42,330},{92,330},{42,388},{92,388},{42,446},{92,446}};
+	private int[] hasBeenWaiting = new int[]{0,0,0,0,0,0};
 	private int[][] sushiRecipe = new int[][]{{0,2,1,0,0,0},{0,1,1,1,0,0},{0,1,1,2,0,0}};
-	private boolean[] beingDone = new boolean[]{false,false,false,false,false,false};
+	private int[] previousState = new int[]{0,0,0,0,0,0};
 	
 	
 	private static Robot r;
@@ -84,57 +85,112 @@ public class Player {
 		Thread.sleep(100);
 	}
 	
+	
+	private void removeAssiete(int i) throws InterruptedException {
+		cv.mouseLeftClick(this.customerPosition[i][0] + this.gameRegion.x + 41, this.customerPosition[i][1] + this.gameRegion.y + 47);
+		Thread.sleep(1000);
+	}
+	
+	private void MakeSushi(int sushiType) throws InterruptedException {
+		// Pour chaque ingrédient
+		// (int) nbr  : contient le nombre 
+		// (int) refIngredient : contient la référence de l'ingrédient en cours
+		
+		int refIngredient = 0;
+		for(int nbr : this.sushiRecipe[sushiType]) {
+			int s = 0;
+			while(s < nbr) {
+				cv.mouseLeftClick(this.sushiPosition[refIngredient][0] + this.gameRegion.x, this.sushiPosition[refIngredient][1] + this.gameRegion.y);
+				Thread.sleep(500);
+				
+				s++;
+			}
+			
+			
+			refIngredient++;
+		}
+		
+		// Puis Validation
+		Thread.sleep(1000);
+		cv.mouseLeftClick(this.gameRegion.x+210, 384 + this.gameRegion.y);
+		Thread.sleep(2000);
+	}
+	
+	private ArrayList<Integer> checkStock(int recipe) {
+		
+		ArrayList<Integer> need = new ArrayList<Integer>();
+		
+		for(int i = 0;i<6;i++) {
+			if(this.stocks[i] < this.sushiRecipe[recipe][i]) {
+				need.add(i);
+			}
+		}
+		
+		return need;
+	}
+	
+	private void handleStock(int i) {
+		ArrayList<Integer >need = this.checkStock(i);
+			if(need.size() > 0 ) {
+				for (int j = 0; j < need.size(); j++) {
+					int ingredient = need.get(j);
+					
+					this.replenish(ingredient);
+				}
+				
+				Thread.sleep(7000);
+			}
+		}
+	
+	private void replenish(int i) {
+		// Telephone : 590, 355;
+		// Toppings : 590,275;
+		// Rice : 590, 295;
+		// Gros bouton riz : 542,275;
+		
+		
+	}
+	
 	private void checkAllCustomers() throws IOException, InterruptedException {
 		for(int i=0;i<6;i++) {
-
-
-			// Dans le cas où on a pas déjà envoyé le sushi
-			if(this.beingDone[i] == false) {
-				
-				// Si on a un client
+			if(this.previousState[i] < 2) {
 				if(checkACustomer(i)) {
-						int d = checkSushi(i);
-						
-						// Si on a un sushi
-						if(d > -1) {
-							int refIngredient = 0;
+					// On regarde si le client i a un sushi
+					int sushiType = checkSushi(i); 
+					
+					if(sushiType > -1) {
+						// Monsieur a un sushi
+						if(this.previousState[i] == 0) {
 							
-							// Pour chaque ingrédient
-							// (int) nbr  : contient le nombre 
-							// (int) refIngredient : contient la référence de l'ingrédient en cours
-							for(int nbr : this.sushiRecipe[d]) {
-								
-								
-								int s = 0;
-								while(s < nbr) {
-									cv.mouseLeftClick(this.sushiPosition[refIngredient][0] + this.gameRegion.x, this.sushiPosition[refIngredient][1] + this.gameRegion.y);
-									Thread.sleep(500);
-									
-									s++;
-								}
-								
-								
-								refIngredient++;
-							}
-							
-							// Puis Validation
-							Thread.sleep(1000);
-							cv.mouseLeftClick(this.gameRegion.x+210, 384 + this.gameRegion.y);
-							this.beingDone[i] = true;
-							Thread.sleep(2000);
+							// Monsieur n'avait pas de commande, on va donc lui faire son petit sushi
+							this.MakeSushi(sushiType);
+							this.hasBeenWaiting[i] = 0;
+							this.previousState[i] = 1;
 						}
+						else {
+							// Monsieur avait déjà une commande, on wait pour voir si elle arrive oupa.
+							this.hasBeenWaiting[i]++;
+							
+							// Si ça fait longtemps qu'il attend, on le remet sur la liste.
+							if(this.hasBeenWaiting[i] >= 2) {
+								this.previousState[i] = 0;
+							}
+						}
+					}
+					else {
+						// Monsieur n'a pas demandé de sushi
+						this.previousState[i] = 2;
+					}
 				}
 			}
 			else {
-				// On a déjà envoyé un sushi à ce type
-				if(!this.checkACustomer(i)) {
-					// Si le type s'est tiré on ramasse son assiete
-					cv.mouseLeftClick(this.customerPosition[i][0] + this.gameRegion.x + 41, this.customerPosition[i][1] + this.gameRegion.y + 47);
-					this.beingDone[i] = false;
-					
-					Thread.sleep(1000);
+				// Le type a passé commande et l'a reçu, on check si on peut virer l'assiète
+				if(!checkACustomer(i)) {
+					this.removeAssiete(i);
+					this.previousState[i] = 0;
 				}
 			}
+
 		}
 	}
 	
